b318367587478c543f33cdbe65adb18c9d02fc6c
Ansh Khatod <133363427+Anshkh21@users.noreply.github.com>
2024-04-04 13:05:06 +0000

    Operators

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,7 +1,14 @@
-console.log("Hello New start");
-let age=20
-let name="Ansh"
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof null);
-console.log(typeof undefined);
+// console.log("Hello New start");
+// let age=20
+// let name="Ansh"
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof null);
+// // console.log(typeof undefined);
+// let a=4 
+// let b=a++
+// let c=++a
+// // console.log(b);
+// // console.log(c);
+// console.log(a++);
+// console.log(++a);
